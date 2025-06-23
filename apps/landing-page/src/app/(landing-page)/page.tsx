f1bf130a14984498b355b792131a457b87eea6fd
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-23 20:27:01 -0300

    fix: change entire page ui

--- apps/landing-page/src/app/(landing-page)/page.tsx
+++ apps/landing-page/src/app/(landing-page)/page.tsx
@@ -7,7 +7,7 @@ import { VintageDucks } from './components/vintage-ducks'
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen selection:bg-tangerine-300">
+		<div className="flex flex-col items-center justify-center h-dvh selection:bg-tangerine-300">
 			<HeroActions />
 			<PlayerRecord />
 			<VintageDucks />
