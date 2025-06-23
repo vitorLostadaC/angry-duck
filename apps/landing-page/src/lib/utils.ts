3595d2ed7ba78f838cceb9a3b18783b5aeb29e48
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-23 20:26:52 -0300

    feat: put mobile as unsupported

--- apps/landing-page/src/lib/utils.ts
+++ apps/landing-page/src/lib/utils.ts
@@ -26,6 +26,10 @@ export type Os = 'mac' | 'windows' | 'linux'
 export const getMyOs = (): Os | 'unknown' | 'unsupported' => {
 	if (typeof window === 'undefined') return 'unknown'
 	const userAgent = window.navigator.userAgent
+
+	const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)
+	if (isMobile) return 'unsupported'
+
 	if (userAgent.includes('Mac')) return 'mac'
 	if (userAgent.includes('Windows')) return 'windows'
 	if (userAgent.includes('Linux')) return 'linux'
