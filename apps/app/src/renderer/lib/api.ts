9b5aa17d67829674cdbe81320a50f2d32cfdf9b3
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-29 20:34:35 -0300

    feat: implement logout when JWT expires

--- apps/app/src/renderer/lib/api.ts
+++ apps/app/src/renderer/lib/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { env } from '../env'
+import { getStoreOptions } from '../requests/electron-store/options'
 import type { ApiError } from '../types/api'
+import { queryClient } from './react-query'
 
 export const api = axios.create({
 	baseURL: env.VITE_API_URL,
@@ -12,6 +14,14 @@ api.interceptors.response.use(
 	(e) => {
 		console.log(e)
 		const error = e.response?.data as ApiError
+		if (error.error === 'Unauthorized') {
+			queryClient.invalidateQueries(getStoreOptions())
+			window.api.store.updateStore({
+				store: {
+					auth: null
+				}
+			})
+		}
 		throw new Error(error.error, { cause: error.message })
 	}
 )
