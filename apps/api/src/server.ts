b314fd333ccd5adcd6e00927a55d8d8504495abe
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-30 21:43:19 -0300

    fix: backend route

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -40,7 +40,7 @@ fastify.register(routes)
 
 fastify.setErrorHandler(errorHandler)
 
-fastify.listen({ port: 3333 }, (err) => {
+fastify.listen({ port: 3333, host: '0.0.0.0' }, (err) => {
 	if (err) {
 		fastify.log.error(err)
 		process.exit(1)
